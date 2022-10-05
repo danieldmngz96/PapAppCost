64ec4787f123efafe22b89138e5b7134c6fa5769
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2022-10-05 12:00:55 -0500

    creacion de footer y header
    se creo header y footer con angular matirial los modulos

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { LayoutComponent } from './app/layout/layout.component';
 
 
 const routes: Routes = [
-  { path: 'HomePapApp', component: AppComponent }
-];
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/HomePapApp',
+        pathMatch: 'full',
+      },
+
+  ],
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,23 @@ import { MaterialModule } from './app/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './app/shared/header/header.component';
+import { FooterComponent } from './app/shared/footer/footer.component';
+
 
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule],
+    FormsModule,
+   ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
