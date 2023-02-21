6d92c5f501604f8aa0f7e8138a46d2aa291b60d5
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-02-21 17:18:07 -0500

    cambios en home componeet

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -8,7 +8,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent implements OnInit {
