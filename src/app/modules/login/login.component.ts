faed178ed9f7aea7c860e0f037e021cc8282c92b
Daniel Dominguez Dominguez <danarturodo@hotmail.com>
2023-02-28 12:31:39 -0500

    arreglos de login logica localstorage y microsservicios

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -10,16 +10,21 @@ import { Router } from '@angular/router';
 import { RegistroComponent } from '../registro/registro.component';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginService } from 'src/app/service/login.service';
-
+import swal from'sweetalert2';
+interface User {
+  username: string;
+  [password: string]: any;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  username: any = '';
-  password: any = '';
-
+  username: any = 'danarturodo@hotmail.com';
+  password: any = 'Asas123*';
+  //form!: FormGroup;
+  loading = false;
   submitted = false;
   form: FormGroup = this.formBuilder.group({
     username: ['', [Validators.required, Validators.email]],
@@ -38,8 +43,19 @@ export class LoginComponent implements OnInit {
     public dialog: MatDialog,
     public loginservice: LoginService
   ) {}
+  public saveData(username: string, password: string) {
+    localStorage.setItem(username, password);
+  }
+  public getData(password: string) {
+    return localStorage.getItem(password);
+  }
 
-  ngOnInit(): void {}
+  public clearData() {
+    localStorage.clear();
+  }
+  ngOnInit(): void {
+
+  }
   get f() {
     return this.form.controls;
   }
@@ -48,22 +64,29 @@ export class LoginComponent implements OnInit {
       this.submitEM.emit(this.form.value);
     }
   } */
+  //redirige a welcome
   goToLogin() {
     this.router.navigate(['/Welcome']);
   }
 
   @Output() submitEM = new EventEmitter();
-
+  //este on submit es para manejarlo quemado por localstrorage
   onSubmit() {
     // Verificar si el usuario y la contraseña son correctos
-    if (this.username === 'usuario' && this.password === 'contraseña') {
-      // Almacenar el nombre de usuario en localStorage
-      localStorage.setItem('username', this.username);
+    if (this.username  && this.password ) {
+      // Almacenar el nombre  y contraseña de usuario en localStorage
+      localStorage.setItem('', this.username);
+      localStorage.setItem('', this.password);
       // Redirigir al usuario a la página de inicio
       this.router.navigate(['/Welcome']);
     } else {
       // Mostrar un mensaje de error si el usuario y/o la contraseña son incorrectos
-      alert('Nombre de usuario y/o contraseña incorrectos.');
+      swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Hubo un error usuario o contraseña con son correctos'
+      })
+
     }
   }
   get isNameUserInvalid() {
@@ -87,7 +110,7 @@ export class LoginComponent implements OnInit {
       width: '550px',
     });
   }
-
+  //este metodo es el que se comunica con el microservcio
   login() {
     const user = { username: this.username, password: this.password };
     this.loginservice.login(user).subscribe((data) => {
@@ -100,8 +123,8 @@ export class LoginComponent implements OnInit {
     });
   }
 
-    //Obtiene el formControl del login
-    getLoginControl(index: number): FormGroup {
-      return this.form.controls[index] as FormGroup;
-    }
+  //Obtiene el formControl del login
+  getLoginControl(index: number): FormGroup {
+    return this.form.controls[index] as FormGroup;
+  }
 }
